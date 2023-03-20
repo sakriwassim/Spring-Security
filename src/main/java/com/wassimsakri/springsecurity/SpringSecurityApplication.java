9be70532c3599f1215d40cf5c3d0e279ccc65ac1
sakriwassim <sakriwassim@gmail.com>
2023-03-20 01:08:57 +0100

    add example of users

--- src/main/java/com/wassimsakri/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/wassimsakri/springsecurity/SpringSecurityApplication.java
@@ -1,13 +1,28 @@
 package com.wassimsakri.springsecurity;
 
+import com.wassimsakri.springsecurity.sec.entity.AppRole;
+import com.wassimsakri.springsecurity.sec.entity.AppUser;
+import com.wassimsakri.springsecurity.sec.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(AccountService accountService){
+		return args -> {
+			accountService.addNewRole(new AppRole(null,"USER"));
+
+			accountService.addNewUser(new AppUser(null,"user1","123",new ArrayList<>()));
+		};
 	}
 
 }
